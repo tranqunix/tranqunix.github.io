e170606ef897f2707cb754ab8272538fec2de25a
Tranqunix <35797223+tranqunix@users.noreply.github.com>
2018-03-09 19:37:48 +0300

    Revert "Added new font, general container for pages, loaded new footer"
    This reverts commit 330265b8bbfb048ab5b15b3ee46a6734eaf2db18.

--- js/js.js
+++ js/js.js
@@ -46,23 +46,3 @@ $('.team-grid').slick({
     }
   ]
 });
-
-
-$("#iconfor").hover(function() {
-      var src = ($(this).attr("src") === "images/69366.png")
-                    ? "images/693662.png" 
-                    : "images/69366.png";
-      $(this).attr("src", src);
-});
-$("#iconfor2").hover(function() {
-      var src = ($(this).attr("src") === "images/img_2644.png")
-                    ? "images/img_26442.png" 
-                    : "images/img_2644.png";
-      $(this).attr("src", src);
-});
-$("#iconfor3").hover(function() {
-      var src = ($(this).attr("src") === "images/58-youtube-512.png")
-                    ? "images/58-youtube-5122.png" 
-                    : "images/58-youtube-512.png";
-      $(this).attr("src", src);
-});
